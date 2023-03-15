103c99ec3eaa68ab328f2b13cb918f6d781ab09c
bo <bo.bratsberg@funkweb.org>
2023-03-15 13:16:04 +0100

    msg

--- flask_tutorial/flaskr/db.py
+++ flask_tutorial/flaskr/db.py
@@ -22,9 +22,13 @@ def init_db():
 	db = get_db()
 
 	with current_app.open_resource("schema.sql") as f:
-		db.executescript(f.read().decode("uft8"))
+		db.executescript(f.read().decode("utf8"))
 
 @click.command("init-db")
 def init_db_command():
 	init_db()
 	click.echo("Initialized the database")
+
+def init_app(app):
+	app.teardown_appcontext(close_db)
+	app.cli.add_command(init_db_command)
