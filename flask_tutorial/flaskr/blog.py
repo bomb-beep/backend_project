013f327a17453b646f709a9cfbe50279400db08f
bo <bo.bratsberg@funkweb.org>
2023-03-20 12:30:55 +0100

    ignore

--- flask_tutorial/flaskr/blog.py
+++ flask_tutorial/flaskr/blog.py
@@ -13,12 +13,12 @@ def index():
     db = get_db()
     posts = db.execute(
         "SELECT p.id, title, body, created, author_id, username"
-        "FROM post p JOIN user u ON p.author_if = u.user_id"
-        "ORDER BY created DESC"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        " ORDER BY created DESC"
 	).fetchall()
     return render_template("blog/index.html",posts=posts)
 
-@bp.route("/create")
+@bp.route("/create",methods=("GET","POST"))
 def create():
 	if request.method == "POST":
 		title = request.form["title"]
@@ -44,8 +44,8 @@ def create():
 def get_post(id,check_author=True):
 	post = get_db().execute(
 		"SELECT p.id, title, body, created, author_id, username"
-		"FROM post p JOIN user u ON p.author_id = u.id"
-		"WHERE p.id = ?",
+		" FROM post p JOIN user u ON p.author_id = u.id"
+		" WHERE p.id = ?",
 		(id,)
 	).fetchone()
 
@@ -69,14 +69,25 @@ def update(id):
 		if not title:
 			error="Title is required"
 
-		if error is not None:
+		if error is None:
 			db = get_db()
 			db.execute(
 				"UPDATE post SET title = ?, body = ? "
-				"WHERE id = ?",
+				" WHERE id = ?",
 				(title,body,id)
 			)
 			db.commit()
 			return redirect(url_for("blog.index"))
+		else:
+			print(error == None)
 		
-	return render_template("blog/template.html",post=post)
+	return render_template("blog/update.html",post=post)
+
+@bp.route("/<int:id>/delete",methods=("POST",))
+@login_required
+def delete(id):
+	get_post(id)
+	db = get_db()
+	db.execute("DELETE FROM post WHERE id = ?",(id,))
+	db.commit()
+	return redirect(url_for("blog.index"))
